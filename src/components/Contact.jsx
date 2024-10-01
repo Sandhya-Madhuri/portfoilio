746076069de63a44d4dcd68cf39e7dcf6273b8ac
sandhya madhuri <101086309+Sandhya-Madhuri@users.noreply.github.com>
2024-10-02 00:14:37 +0530

    Update: Design update and footer updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,39 @@
 import React, { useContext } from "react";
 import "./contact-styles.css";
 import { RefContext } from "./RefContext";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoIosPhonePortrait } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
   const { contactRef } = useContext(RefContext);
   return (
-    <section id="contact" className="contact-section" ref={contactRef}>
-      <h2>Contact Me</h2>
+    <footer id="contact" className="contact-section" ref={contactRef}>
+      <h2 className="contact-text">Contact Me</h2>
       <div>
         <ul className="contact-list">
           <li>
-            email:{" "}
+            {/* email:{" "} */}
             <a href="mailto:sandhyamadhuri789@gmail.com">
-              sandhyamadhuri789@gmail.com
+              <MdOutlineEmail size={20} />
+              <span>sandhyamadhuri789@gmail.com</span>
             </a>
           </li>
           <li>
-            phone number: <a href="tel:+919390556936">+919390556936</a>
+            <a href="tel:+919390556936">
+              <IoIosPhonePortrait size={20} />
+              <span>+919390556936</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/sandhya-madhuri-b54864111/">
+              <FaLinkedin size={20} />
+              <span>SandhyaMadhuri</span>
+            </a>
           </li>
         </ul>
       </div>
-    </section>
+    </footer>
   );
 };
 
